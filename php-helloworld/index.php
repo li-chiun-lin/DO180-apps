a6afec646f485dfee63fc846dcc21ec8f09dd422
Student User <student@workstation.lab.example.com>
2022-08-17 02:45:34 -0400

    msg

--- php-helloworld/index.php
+++ php-helloworld/index.php
@@ -1,3 +1,16 @@
 <?php
 print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
+print "Hello, World! php version is " . PHP_VERSION . "\n";
 ?>
